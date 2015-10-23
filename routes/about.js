e67a0285bcd5f84ad362df8691b6e48a918f76dc
Vidyasagar <lordvoldemort1729@gmail.com>
2015-10-23 14:04:09 +0530

    adding routes for contact and about us

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,12 @@
+/**
+ * Created by doctor on 23/10/15.
+ */
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+    res.render('about', { title: 'About' });
 });
 
 module.exports = router;
