8024fa9d47f7fa86900b54d3cbbd1c5f06f62d31
Vidyasagar <lordvoldemort1729@gmail.com>
2015-12-03 02:39:27 +0530

    meadowlark app

---
+++ meadowlark/meadowlark.js
@@ -0,0 +1,45 @@
+var express = require('express');
+
+var app = express();
+
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+var fortuneCookies = [
+	"Conquer your fears or they will Conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple."
+];
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	var randomFortune = fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
+	console.log(randomFortune);
+	res.render('about', {fortune: randomFortune});
+});
+
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhots:' +
+		app.get('port') + '; press Ctrl-C to termiante.');
+});
